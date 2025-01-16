61a1f59a078aba5b24e16d950617839b4aaf607c
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2025-01-16 13:57:16 -0300

    add featured products section, add offer section, add footer

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,18 +2,19 @@ import { BiSearch, BiShoppingBag, BiHeart, BiUser } from 'react-icons/bi'
 
 export const Header = () => {
     return (
-        <div className="flex items-center bg-white h-16 px-10">
+        <div className="flex items-center bg-gray-50 h-16 px-10">
             <div className="flex justify-between items-center w-full">
                 <span>Logo</span>
-                <div className="h-[50px] flex items-center bg-white rounded-lg border border-gray-300 p-2 gap-2">
+                {/* Search Bar */}
+                <div className="flex items-center bg-white rounded-lg border border-gray-300 p-2 gap-2">
                     <BiSearch className="size-5"/>
                     <input 
                         type="text" 
-                        className="w-[400px] h-[30px] rounded outline-none"
+                        className="w-96 rounded outline-none"
                         placeholder="Search products..."
                     />
                 </div>
-                <nav className="flex gap-2">
+                <nav className="flex gap-6">
                     <span><BiHeart className="size-6"/></span>
                     <span><BiShoppingBag className="size-6"/></span>
                     <span><BiUser className="size-6"/></span>
--- src/components/pages-components/Home/Content/index.jsx
+++ src/components/pages-components/Home/Content/index.jsx
@@ -1,31 +1,117 @@
+import Image from "next/image"
+
 import electronics from "@/assets/electronics.jpg"
+import categoryElectronics from "../../../../assets/categoryElectronics.png"
 
 export const Content = () => {
-    return (
-        <div>
 
+    console.log(categoryElectronics)
+    return (
+        <div className="bg-gray-50">
             {/* Hero Section*/}
             <div className="flex items-center bg-blue-600 h-96 px-10">
                 <div className="flex items-start flex-col gap-y-4 ">
                     <h1 className="text-4xl font-bold text-white">Summer Collection 2025 </h1> 
                     <h2 className="text-1xl font-semibold text-white">Discover the latest trends and amazing deals</h2>
-                    <button className="bg-white px-6 py-2 rounded-lg font-semibold">Shop now</button>
+                    <button
+                        className="bg-white px-6 py-2 rounded-lg font-semibold text-blue-600"
+                    >
+                        Shop now
+                    </button>
                 </div>
             </div>
 
             {/* Categories Section*/}
-            <div className="mt-8 px-10 mb-8">
-                <h2 className="text-2xl font-semibold ">Shop by Category</h2>
+            <div className="py-12 px-10">
+                <h2 className="text-2xl font-semibold">Shop by Category</h2>
 
                 {/* Categories*/}
-                <div className="flex flex-wrap gap-4 mt-8">
-                    <div className="w-[24%] h-52">
-                        <img src={electronics} alt="a" />
-                        <h2>Electronics</h2>
+                <div className="flex flex-wrap justify-between mt-8">
+                    <div className="w-[23%] h-60 flex justify-center items-center bg-slate-500 rounded-lg">
+                        <h2 className="text-white text-xl font-semibold">Electronics</h2>
+                    </div>
+                    <div className="w-[23%] h-60 flex justify-center items-center bg-slate-500 rounded-lg">
+                        <h2 className="text-white text-xl font-semibold">Electronics</h2>
+                    </div>
+                    <div className="w-[23%] h-60 flex justify-center items-center bg-slate-500 rounded-lg">
+                        <h2 className="text-white text-xl font-semibold">Electronics</h2>
+                    </div>
+                    <div className="w-[23%] h-60 flex justify-center items-center bg-slate-500 rounded-lg">
+                        <h2 className="text-white text-xl font-semibold">Electronics</h2>
                     </div>
                 </div>
-                
             </div>
+
+            {/* Featured Products Section */}
+            <div className="py-12 px-10">
+                <h2 className="text-2xl font-semibold" >Featured Products</h2>
+
+                {/* Featured Products */}
+                <div className="flex justify-between gap-x-20 mt-8">
+                    <div 
+                        className="w-[20%] h-70 p-4 rounded-lg shadow-sm hover:shadow-md hover:cursor-pointer transition-shadow duration-200"
+                    >
+                        <div className="h-60 mb-4">
+                            <Image src={categoryElectronics} alt="teste" className="object-cover w-full h-full rounded-lg" />
+                        </div> 
+                        <div className="h-10">
+                            <h3 className="font-medium text-lg mb-2">Wireless Headphones</h3>
+                            <span className="text-blue-600 font-semibold">$89.99</span>
+                        </div>
+                    </div>    
+                    <div 
+                        className="w-[20%] h-70 p-4 rounded-lg shadow-sm hover:shadow-md hover:cursor-pointer transition-shadow duration-200"
+                    >
+                        <div className="h-60 mb-4 ">
+                            <Image src={categoryElectronics} alt="teste" className="object-cover w-full h-full rounded-lg" />
+                        </div> 
+                        <div className="h-10">
+                            <h3 className="font-medium text-lg mb-2">Wireless Headphones</h3>
+                            <span className="text-blue-600 font-semibold">$89.99</span>
+                        </div>
+                    </div>    
+                    <div 
+                        className="w-[20%] h-70 p-4 rounded-lg shadow-sm hover:shadow-md hover:cursor-pointer transition-shadow duration-200"
+                    >
+                        <div className="h-60 mb-4">
+                            <Image src={categoryElectronics} alt="teste" className="object-cover w-full h-full rounded-lg" />
+                        </div> 
+                        <div className="h-10">
+                            <h3 className="font-medium text-lg mb-2">Wireless Headphones</h3>
+                            <span className="text-blue-600 font-semibold">$89.99</span>
+                        </div>
+                    </div>    
+                    <div 
+                        className="w-[20%] h-70 p-4 rounded-lg shadow-sm hover:shadow-md hover:cursor-pointer transition-shadow duration-200"
+                    >
+                        <div className="h-60 mb-4">
+                            <Image src={categoryElectronics} alt="teste" className="object-cover w-full h-full rounded-lg" />
+                        </div> 
+                        <div className="h-10">
+                            <h3 className="font-medium text-lg mb-2">Wireless Headphones</h3>
+                            <span className="text-blue-600 font-semibold">$89.99</span>
+                        </div>
+                    </div>     
+                </div>
+            </div>
+            
+            <div>
+                <div className="bg-gray-900 py-16 text-white">
+                    <div className="flex justify-center">
+                        <div className="flex flex-col items-center">
+                            <h2 className="text-3xl font-bold mb-4">Special Offer</h2>
+                            <h3 className="text-xl mb-8">Get 20% off on your first purchase</h3>
+                            <button className="px-6 py-2 rounded-lg font-semibold bg-white text-gray-900">
+                                Shop Now
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <footer className="bg-gray-50 py-16 px-10">
+                    a
+                </footer>
+            </div>
+            
         </div>
     )
 }
