8decdb79fe1aa7ad15b89c20ea799a6df23c1134
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2025-03-26 13:45:19 -0300

    fix page doesnt loading

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1db3c1c9e553df77b20e6bc871850ee37f83f7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57743f977917b7b1758b093944def94f001ee5d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
